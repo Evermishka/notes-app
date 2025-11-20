ea6223bfb10a90b64b4e5e3d35441df071a208c6
Ermishina Evgeniya <evermishka@yandex.ru>
2025-11-20 12:30:32 +0300

    feat: настроить ESLint + Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'build', '.vite', 'coverage', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -14,10 +16,20 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      react,
+      import: importPlugin,
+    },
+    rules: {
+      eqeqeq: ['error', 'always'], // js.recommended → 'warn'
+      'no-console': ['warn', { allow: ['warn', 'error'] }], // По умолчанию запрет всех, разрешаем warn/error
+      'react/jsx-key': 'error', // key в списках
+    },
   },
-])
+]);
