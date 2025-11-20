6b216607dc18e29bb31643f8eb56090490175c74
Ermishina Evgeniya <evermishka@yandex.ru>
2025-11-20 11:35:50 +0300

    feat: инициализировать приложение с Vite + React + TypeScript + FSD архитектурой

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+export function App() {
+  return (
+    <MantineProvider>
+      <h1>Hello World</h1>
+    </MantineProvider>
+  );
+}
+
