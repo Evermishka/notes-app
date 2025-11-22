2385aaa899cc0a730db0ace2662d5d9d81d83e02
Ermishina Evgeniya <evermishka@yandex.ru>
2025-11-22 18:32:22 +0300

    refactor: вынести провайдеры в отдельный компонент Providers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,10 @@
+import { Providers } from './ui';
 import { Router } from './router';
-import { AuthProvider } from '@/features/auth';
-import { NotesProvider } from '@/features/notes-crud';
-import { ViewEditorProvider } from '@/features/notes-view-editor';
-import { MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
 
 export const App = () => {
   return (
-    <MantineProvider>
-      <AuthProvider>
-        <NotesProvider>
-          <ViewEditorProvider>
-            <Router />
-          </ViewEditorProvider>
-        </NotesProvider>
-      </AuthProvider>
-    </MantineProvider>
+    <Providers>
+      <Router />
+    </Providers>
   );
 };
