f454c3ba0099fbcd577ef8ba81126ea1775b95f2
Ermishina Evgeniya <evermishka@yandex.ru>
2025-11-29 10:25:07 +0300

    feat: добавить поддержку отключения оверлея HMR в конфигурации Vite и регистрировать service worker только в продакшене

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
